07e3445656a7c3771573f27838565ba45bb94dcb
Grace Eileen Bass <grace.eilee.bass@gmail.com>
2025-08-18 00:25:36 +0300

    Add Student and ChattyStudent classes with inheritance, super(), and method overriding

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,22 @@
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
 
 class ChattyStudent(Student):
-    pass
+    from student import Student
+
+class ChattyStudent(Student):
+    def hello(self):
+        # call parent method first
+        super().hello()
+        # add chatty message
+        print("How are you doing today? I'm okay, but I'm kind of tired. "
+              "Did you watch The Walking Dead last night? You didn't?! "
+              "Oh man, it was so crazy! What, you don't want any spoilers? "
+              "Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        # call super() 10 times
+        for _ in range(10):
+            super().raise_hand()
+
